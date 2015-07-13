4aa2895a55ca40c6489b1f1cac53b3c21c81c6f0
Helton Alves <helton99@hotmail.com>
2015-07-13 09:00:14 -0300

    Changed the TEMPLATE_PATH variable on settings file.

--- src/settings.py
+++ src/settings.py
@@ -3,4 +3,4 @@ import os
 
 
 PROJECT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'sdfasdfasd')
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'views')
